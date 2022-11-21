0fbfa83050e5f28049c882d3e3e8735f78829531
bornbygoogle <bornbygoogle@yahoo.com>
2022-11-21 23:56:58 +0100

    Blazorise template "Zay Shop"

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using BlazorApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddWMBSC();
 
 await builder.Build().RunAsync();
